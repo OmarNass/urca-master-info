1a976c232e50d522283391e41ca68321cde2168a
Omar Nassim <30617376+OmarNass@users.noreply.github.com>
2020-08-27 22:21:51 +0200

    Updates

---
+++ precache-manifest.40e8adf04f0cf4e681d6bfa02fed03b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30981c15fbce4bf49d4b91f7937bb190",
+    "url": "/urca-master-info/index.html"
+  },
+  {
+    "revision": "e8c4079950f766184d4c",
+    "url": "/urca-master-info/static/css/2.fa008d51.chunk.css"
+  },
+  {
+    "revision": "4a6a45aa1ef499fed34b",
+    "url": "/urca-master-info/static/css/main.b1f1e749.chunk.css"
+  },
+  {
+    "revision": "e8c4079950f766184d4c",
+    "url": "/urca-master-info/static/js/2.39c97a8a.chunk.js"
+  },
+  {
+    "revision": "4548a5dca736863cb7fa383d81225302",
+    "url": "/urca-master-info/static/js/2.39c97a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6a45aa1ef499fed34b",
+    "url": "/urca-master-info/static/js/main.1188348c.chunk.js"
+  },
+  {
+    "revision": "b6cdb179fa49bedff00e",
+    "url": "/urca-master-info/static/js/runtime-main.7fd33aff.js"
+  },
+  {
+    "revision": "45943c86c6f1efcedb4e8ad4eee830fd",
+    "url": "/urca-master-info/static/media/bg.45943c86.jpg"
+  },
+  {
+    "revision": "357938b407fa3b1371888beca733ac31",
+    "url": "/urca-master-info/static/media/logo.357938b4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/urca-master-info/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/urca-master-info/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/urca-master-info/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/urca-master-info/static/media/slick.f97e3bbf.svg"
+  }
+]);
